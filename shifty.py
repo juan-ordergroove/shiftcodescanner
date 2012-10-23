b7c04a98c693058053d057b66133a09b6d5364c8
Juan Gutierrez <juanny.gee@gmail.com>
2012-10-23 09:26:55 -0400

    changed date limiting from 'yesterday' to two hours ago; removed direct references to keys & secrets for oauth - they live in a file now

--- shifty.py
+++ shifty.py
@@ -1,16 +1,21 @@
-import twitter, re
+import twitter, re, os
 from datetime import *
 
-api = twitter.Api(consumer_key='', consumer_secret='', access_token_key='', access_token_secret='')
+dir_name = os.path.dirname(__file__)
+c_k = open(os.path.join(dir_name, '../c.k')).read()
+c_s = open(os.path.join(dir_name, '../c.s')).read()
+a_t_k = open(os.path.join(dir_name, '../a_t.k')).read()
+a_t_s = open(os.path.join(dir_name, '../a_t.s')).read()
+
+api = twitter.Api(consumer_key=c_k, consumer_secret=c_s, access_token_key=a_t_k, access_token_secret=a_t_s)
 users = ['DuvalMagic', 'gearboxsoftware']
-yesterday = (datetime.now() - timedelta(1)).date()
+two_hours_ago = datetime.today() + timedelta(hours=-2)
 
 for user in users:
 	timeline = api.GetUserTimeline(user)
 	for tweet in timeline:
-		created = datetime.strptime(tweet.created_at, '%a %b %d %H:%M:%S +0000 %Y').date()
-		print created, yesterday, created < yesterday
-		if created < yesterday: continue
-		
+		created = datetime.strptime(tweet.created_at, '%a %b %d %H:%M:%S +0000 %Y')
+		if two_hours_ago > created: continue
+
 		m = re.search('X360: [A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}', tweet.text)
 		if m and m.group(0): print "Found!", m.group(0)
