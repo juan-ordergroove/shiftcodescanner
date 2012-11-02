df965ddde45aef4277c0c5ede3ed22706d94a78d
Juan Gutierrez <juanny.gee@gmail.com>
2012-11-02 18:36:55 -0400

    implemented python logger

--- shifty.py
+++ shifty.py
@@ -1,14 +1,19 @@
-import re, os, codecs, smtplib, string, twitter, traceback
+import codecs, logging, os, re, smtplib, string, twitter, traceback
 from datetime import *
 from types import *
 
+logging.basicConfig(filename='/tmp/twitter_shift_code_scanner.log'
+                    , format='%(asctime)s %(message)s'
+                    , datefmt='%m/%d/%Y %I:%M:%S %p'
+                    , level=logging.INFO)
+
 def write_codes(codes):
-    print 'writing codes...'
+    logging.info('writing codes...')
     fout = codecs.open('codes.log', 'w', 'utf-8')
     fout.write(codes)
     fout.close()
     
-    print 'emailing codes...'
+    logging.info('emailing codes...')
     p = open(os.path.join(dir_name, '../g.p')).read()
     from_addrs = 'juanny.gee@gmail.com'
     to_addrs = ['juanny.gee@gmail.com']
@@ -36,7 +41,7 @@ users = ['DuvalMagic', 'gearboxsoftware']
 two_hours_ago = datetime.utcnow() + timedelta(hours=-2)
 codes = ''
 
-print 'scanning users...'
+logging.info('scanning users...')
 for user in users:
 	timeline = api.GetUserTimeline(user)
 	for tweet in timeline:
@@ -51,11 +56,11 @@ try:
         f = codecs.open('codes.log', 'r', 'utf-8')
         past_codes = f.read()
 
-        if past_codes == codes: print "these codes have already been delivered"
+        if past_codes == codes: logging.info("these codes have already been delivered")
         else: write_codes(codes)
         f.close()
     else:
-        print 'no new codes'
+        logging.info('no new codes')
         pass
 except:
     write_codes(codes)
