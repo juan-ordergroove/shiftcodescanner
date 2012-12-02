cc0f9df9f125000e6297e781f632cf19da49cb82
Juan Gutierrez <juanny.gee@gmail.com>
2012-12-02 16:31:18 -0500

    updated how codes are scanned and historical scans are stored; now works more as a notification system for any system's shift code posting, and should continue to do so, regardless of the posting format

--- shifty.py
+++ shifty.py
@@ -1,4 +1,5 @@
-import codecs, logging, os, re, smtplib, string, simplejson, twitter, traceback
+import codecs, copy, logging, os, re
+import smtplib, string, simplejson, twitter, traceback
 from datetime import *
 from types import *
 
@@ -12,6 +13,10 @@ logging.basicConfig(filename='/var/log/shiftscanner/scan.log'
                     , format='%(asctime)s %(message)s'
                     , datefmt='%m/%d/%Y %I:%M:%S %p'
                     , level=logging.INFO)
+
+def list_diff(a, b):
+    b = set(b)
+    return [aa for aa in a if aa not in b]
 
 def send_mail(from_addrs, to, subject, body):
     msg = "From: %s\r\nTo: %s\r\nSubject: %s\r\nDate: %s\r\n\r\n%s" % (
@@ -30,22 +35,23 @@ def send_mail(from_addrs, to, subject, body):
     
 def write_codes(codes):
     logging.info('writing codes...')
+    logging.info(simplejson.dumps(codes))
     fout = codecs.open('codes.log', 'w', 'utf-8')
-    fout.write(codes)
+    fout.write(simplejson.dumps(codes))
     fout.close()
     
     logging.info('emailing codes...')
     send_mail(shift_config.get('from_addrs')
             , shift_config.get('mailing_list')
-            , 'New SHIFT codes posted'
+            , 'SHIFT code notification'
             , codes
             )
 
 def scan_for_codes():
-    codes = ''
+    codes = []
     past_codes = False
     
-    two_hours_ago = datetime.utcnow() + timedelta(hours=-2)
+    two_hours_ago = datetime.utcnow() + timedelta(minutes=-30)
     api = twitter.Api(consumer_key=shift_config.get('c_k')
             , consumer_secret=shift_config.get('c_s')
             , access_token_key=shift_config.get('at_k')
@@ -59,27 +65,19 @@ def scan_for_codes():
             created = datetime.strptime(tweet.created_at, '%a %b %d %H:%M:%S +0000 %Y')
             if two_hours_ago > created: continue
 
-            m = re.search('.*360.*[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}', tweet.text)
-            if m and m.group(0): codes += tweet.text
+            m = re.findall('[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}', tweet.text)
+            if m: codes += [tweet.text]
     
-    if codes:
-        try:
-            f = codecs.open('codes.log', 'r', 'utf-8')
-            past_codes = f.read()
-            f.close()
-        except:
-            logging.info(traceback.format_exc())
-            msg = {'codes': codes, 'traceback': traceback.format_exc() }
-            send_mail('juanny.gee@gmail.com'
-                    , ['juanny.gee@gmail.com']
-                    , 'code scan failure'
-                    , simplejson.dumps(msg)
-                    )
-        
-        if past_codes == codes: logging.info("these codes have already been delivered")
-        else: write_codes(codes)
-    else:
-        logging.info('no new codes')
+    try:
+        f = codecs.open('codes.log', 'r', 'utf-8')
+        past_codes = simplejson.loads(f.read())
+        f.close()
+    except:
+        past_codes = []
+
+    new_codes = list_diff(codes, past_codes)
+    if new_codes: write_codes(new_codes)
+    else: logging.info('no new codes')
 
 if __name__ == '__main__':
     scan_for_codes()
