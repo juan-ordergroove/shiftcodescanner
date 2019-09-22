b789adc8713334455b7f4b77f735d01b311701d4
Juan Gutierrez <juanny.gee@gmail.com>
2019-09-22 10:59:50 -0400

    Add simple SHIFT code matcher class
    Keeping it a class for future extensibility.

--- shift_scanner/shift_scanner/matcher.py
+++ shift_scanner/shift_scanner/matcher.py
@@ -1,8 +1,11 @@
 #-*- coding: utf-8 -*-
 
+import re
+
 
 class ShiftCodeMatcher(object):
-    #SHIFT_CODE_REGEX = '[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}'
+    SHIFT_CODE_PATTERN = re.compile('[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}-[A-Z0-9]{5}')
 
-    def contains_shift_code(self, *args, **kwargs):
-        return True
+    @classmethod
+    def contains_shift_code(cls, text, *args, **kwargs):
+        return cls.SHIFT_CODE_PATTERN.findall(text)
--- shift_scanner/tests/test_scanner.py
+++ shift_scanner/tests/test_scanner.py
@@ -5,8 +5,12 @@ import unittest
 from shift_scanner.matcher import ShiftCodeMatcher
 
 class ShiftCodeMatcherUnitTest(unittest.TestCase):
-    def setUp(self):
-        self.matcher = ShiftCodeMatcher()
+    def test_when_a_string_does_not_contain_a_SHIFT_code_the_matcher_returns_false(self):
+        self.assertFalse(
+            ShiftCodeMatcher.contains_shift_code("This does not contain a shift code")
+        )
 
-    def test_a_string_that_does_not_contain_a_shift_code_return_false(self):
-        self.assertFalse(self.matcher.contains_shift_code("This does not contain a shift code"))
+    def test_when_a_string_contains_a_SHIFT_code_the_matcher_returns_true(self):
+        self.assertTrue(
+            ShiftCodeMatcher.contains_shift_code("This has a shift code: AAAAA-11111-BBBBB-22222-CCCCC")
+        )
