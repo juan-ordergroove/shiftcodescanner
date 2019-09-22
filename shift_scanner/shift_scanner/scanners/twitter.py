c4adf9f7c3c878826b4ce67b8e38f0c1c7f1c258
Juan Gutierrez <juanny.gee@gmail.com>
2019-09-22 17:01:35 -0400

    Return a list of URLs in twitter scanner

--- shift_scanner/shift_scanner/scanners/twitter.py
+++ shift_scanner/shift_scanner/scanners/twitter.py
@@ -16,18 +16,22 @@ class Twitter(object):
         )
 
     def filter(self, *args, **kwargs):
-        tweets_with_shift_codes = []
+        twitter_urls_with_shift_codes = []
         tweets = self._api.GetUserTimeLine(screen_name='dgShiftCodes')
         for tweet in tweets:
             if self._tweet_is_older_than_two_hours(tweet):
                 continue
 
             if contains_shift_code(tweet.text):
-                tweets_with_shift_codes.append(tweet)
-        return tweets_with_shift_codes
+                twitter_urls_with_shift_codes.append(self._build_url_from_tweet(tweet))
+        return twitter_urls_with_shift_codes
 
     @staticmethod
     def _tweet_is_older_than_two_hours(tweet):
         now = int(time.time())
         delta = now - tweet.created_at_in_seconds
         return (delta / 60 / 60) >= 2
+
+    @staticmethod
+    def _build_url_from_tweet(tweet):
+        return 'https://twitter.com/{}/status/{}'.format(tweet.user.screen_name, tweet.id)
--- shift_scanner/tests/test_twitter_scanner.py
+++ shift_scanner/tests/test_twitter_scanner.py
@@ -18,24 +18,13 @@ class TwitterScannerUnitTests(unittest.TestCase):
         self.mock_tweets = [Status.NewFromJsonDict({
             "text": c,
             "created_at": self.created_at_now
-        }) for c in ['a', 'b', 'c']]
+        }) for i, c in enumerate(['a', 'b', 'c'])]
 
     def test_twitter_scanner_returns_an_empty_list_when_no_tweets_contain_shift_codes(self, mock_twitter):
         mock_twitter.Api.return_value.GetUserTimeLine.return_value = self.mock_tweets
 
         twitter_scanner = Twitter()
         self.assertEqual([], twitter_scanner.filter())
-
-    def test_twitter_scanner_returns_a_list_of_tweets_that_contain_shift_codes(self, mock_twitter):
-        tweet_with_shift_code = Status.NewFromJsonDict({
-            "text": "There's a shift code here! {}".format(self.MOCK_SHIFT_CODE),
-            "created_at": self.created_at_now
-        })
-        self.mock_tweets.append(tweet_with_shift_code)
-
-        twitter_scanner = Twitter()
-        mock_twitter.Api.return_value.GetUserTimeLine.return_value = self.mock_tweets
-        self.assertEqual([tweet_with_shift_code], twitter_scanner.filter())
 
     def test_tweets_older_than_two_hours_are_ignored(self, mock_twitter):
         two_hours_ago = datetime.datetime.utcnow() - datetime.timedelta(hours=2)
@@ -48,3 +37,20 @@ class TwitterScannerUnitTests(unittest.TestCase):
         twitter_scanner = Twitter()
         mock_twitter.Api.return_value.GetUserTimeLine.return_value = self.mock_tweets
         self.assertEqual([], twitter_scanner.filter())
+
+    def test_twitter_scanner_returns_a_list_of_links_to_tweets_that_contain_shift_codes(self, mock_twitter):
+        tweet_with_shift_code = Status.NewFromJsonDict({
+            "id": 1000,
+            "user": {"screen_name": "screenname"},
+            "text": "There's a shift code here! {}".format(self.MOCK_SHIFT_CODE),
+            "created_at": self.created_at_now
+        })
+        self.mock_tweets.append(tweet_with_shift_code)
+
+        twitter_scanner = Twitter()
+        mock_twitter.Api.return_value.GetUserTimeLine.return_value = self.mock_tweets
+        expected_url = 'https://twitter.com/{}/status/{}'.format(
+            tweet_with_shift_code.user.screen_name,
+            tweet_with_shift_code.id
+        )
+        self.assertEqual([expected_url], twitter_scanner.filter())
