7871fec953dff4ca5825274bd4f8e871717719b9
Juan Gutierrez <juanny.gee@gmail.com>
2019-09-22 16:16:45 -0400

    Implement twitter scanner
    * Add test coverage reporting

--- shift_scanner/shift_scanner/scanners/twitter.py
+++ shift_scanner/shift_scanner/scanners/twitter.py
@@ -1,19 +1,22 @@
 #-*- coding: utf-8 -*-
 
 import twitter
+from shift_scanner.matcher import contains_shift_code
 
-#from shift_scanner.scanners.base import ShiftScanner
-#
-#
-#class Twitter(ShiftScanner):
-#    def __init__(self, *args, **kwargs):
-#        self._api = twitter.Api(
-#            consumer_key='',
-#            consumer_secret='',
-#            access_token_key='',
-#            access_token_secret=''
-#        )
-#
-#    def _scan(self, *args, **kwargs):
-#        # TODO: replace hardcoded screen name with list of screen names from CFG
-#        tweets = self._api.GetUserTimeLine(screen_name='dgShiftCodes')
+
+class Twitter(object):
+    def __init__(self, *args, **kwargs):
+        self._api = twitter.Api(
+            consumer_key='',
+            consumer_secret='',
+            access_token_key='',
+            access_token_secret=''
+        )
+
+    def filter(self, *args, **kwargs):
+        tweets_with_shift_codes = []
+        tweets = self._api.GetUserTimeLine(screen_name='dgShiftCodes')
+        for tweet in tweets:
+            if contains_shift_code(tweet.text):
+                tweets_with_shift_codes.append(tweet)
+        return tweets_with_shift_codes
--- shift_scanner/tests/test_twitter_scanner.py
+++ shift_scanner/tests/test_twitter_scanner.py
@@ -2,19 +2,30 @@
 
 import unittest
 from unittest import mock
-#from shift_scanner.scanners.twitter import Twitter
-#
-#
-#@mock.patch('shift_scanner.scanners.twitter.twitter')
-#class TwitterScannerUnitTests(unittest.TestCase):
-#    MOCK_SHIFT_CODE = 'AAAAA-11111-BBBBB-22222-CCCCC'
-#    def test_when_set_of_tweets_contain_shift_codes_the_scanner_returns_them(self, mock_twitter):
-#        mock_tweet = mock.Mock() # Based on twitter.Api>GetUserTimeLine objects
-#        mock_tweet.text = "This contains a shift code: {}".format(self.MOCK_SHIFT_CODE)
-#
-#        mock_tweets = [mock_tweet]
-#        mock_twitter.Api.GetUserTimeLine.return_value = mock_tweets
-#
-#        twitter_scanner = Twitter()
-#        results = twitter_scanner.scan()
-#        self.assertEqual(mock_tweets, results)
+from twitter.models import Status
+
+from shift_scanner.scanners.twitter import Twitter
+
+
+@mock.patch('shift_scanner.scanners.twitter.twitter')
+class TwitterScannerUnitTests(unittest.TestCase):
+    MOCK_SHIFT_CODE = 'AAAAA-11111-BBBBB-22222-CCCCC'
+
+    def setUp(self):
+        self.mock_tweets = [Status.NewFromJsonDict({"text": c}) for c in ['a', 'b', 'c']]
+
+    def test_twitter_scanner_returns_an_empty_list_when_no_tweets_contain_shift_codes(self, mock_twitter):
+        mock_twitter.Api.return_value.GetUserTimeLine.return_value = self.mock_tweets
+
+        twitter_scanner = Twitter()
+        self.assertEqual([], twitter_scanner.filter())
+
+    def test_twitter_scanner_returns_a_list_of_tweets_that_contain_shift_codes(self, mock_twitter):
+        tweet_with_shift_code = Status.NewFromJsonDict({
+            "text": "There's a shift code here! {}".format(self.MOCK_SHIFT_CODE)
+        })
+        self.mock_tweets.append(tweet_with_shift_code)
+
+        twitter_scanner = Twitter()
+        mock_twitter.Api.return_value.GetUserTimeLine.return_value = self.mock_tweets
+        self.assertEqual([tweet_with_shift_code], twitter_scanner.filter())
