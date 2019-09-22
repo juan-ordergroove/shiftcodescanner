db9d24730093028ae02398e71a84d6d04cf57562
Juan Gutierrez <juanny.gee@gmail.com>
2019-09-22 09:58:39 -0400

    Begin rewrite
    * Use python 3 instead of 2
    * New package structure

---
+++ shift_scanner/tests/test_twitter_scanner.py
@@ -0,0 +1,9 @@
+ #-*- coding: utf-8 -*-
+
+import unittest
+from shift_scanner.twitter import TwitterScanner
+
+
+class TwitterScannerUnitTests(unittest.TestCase):
+    def test_initialization_raises_an_exception_when_authentication_fails(self):
+        pass
