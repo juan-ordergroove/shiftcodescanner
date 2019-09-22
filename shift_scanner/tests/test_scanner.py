7b7d29e3794b0e24937130ed3215f7f2964c07ea
Juan Gutierrez <juanny.gee@gmail.com>
2019-09-22 11:00:44 -0400

    Spacing tweak

--- shift_scanner/tests/test_scanner.py
+++ shift_scanner/tests/test_scanner.py
@@ -4,6 +4,7 @@ import unittest
 
 from shift_scanner.matcher import ShiftCodeMatcher
 
+
 class ShiftCodeMatcherUnitTest(unittest.TestCase):
     def test_when_a_string_does_not_contain_a_SHIFT_code_the_matcher_returns_false(self):
         self.assertFalse(
